daf47fb46f0d4ceb87fc1b39050f11a3372a8227
Bradley Griffin <bigriff11@gmail.com>
2022-12-13 14:51:59 -0700

    Add functionality to the minutes hand

--- analog/analog.js
+++ analog/analog.js
@@ -1,13 +1,19 @@
 
 let secondHand = document.querySelector(".second");
+let minuteHand = document.querySelector(".minute");
 
 function setDate() {
+    // Create a new date
     let now = new Date()
+    // store the seconds
     let seconds = now.getSeconds()
     let secondDegree = ((seconds / 60) * 360) + 270;
+    // store the minutes
+    let minutes = now.getMinutes()
+    let minuteDegree = ((minutes / 60) * 360) + 270;
+    // change hand
     secondHand.style.transform = `rotate(${secondDegree}deg)`;
-    console.log(seconds)
-
+    minuteHand.style.transform = `rotate(${minuteDegree}deg)`;
 }
 
 setInterval(setDate, 1000);
