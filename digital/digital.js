62a6924935ad4aa2cbed942bc36c00ebe086c8bb
Bradley Griffin <bigriff11@gmail.com>
2022-12-13 18:10:06 -0700

    Add minutes and hours

--- digital/digital.js
+++ digital/digital.js
@@ -1,13 +1,25 @@
 let secondArea = document.querySelector(".second")
+let minuteArea = document.querySelector(".minute")
+let hourArea = document.querySelector(".hour")
 
 
 function setDate() {
-    let now = new Date()
-    let seconds = now.getSeconds()
+    let now = new Date();
+    let seconds = now.getSeconds();
+    let minutes = now.getMinutes();
+    let hours = now.getHours() % 12;
     if (seconds >= 0 && seconds < 10) {
         secondArea.textContent = '0' + seconds
     }
-    else secondArea.textContent = seconds
+    else secondArea.textContent = seconds;
+    if (minutes >= 0 && minutes < 10) {
+        minuteArea.textContent = '0' + minutes
+    }
+    else minuteArea.textContent = minutes;
+    if (hours >= 0 && hours < 10) {
+        hourArea.textContent = '0' + hours
+    }
+    else hourArea.textContent = hours;
 
 }
 
